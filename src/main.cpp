2972a95a51f8bcfbe8a8d9cfacd38adcf0a48a08
Milena Freitas <77834885+MilenaFreitas@users.noreply.github.com>
2022-02-15 09:12:52 -0300

    updateRemoto

--- src/main.cpp
+++ src/main.cpp
@@ -72,6 +72,82 @@ int contar=0;
 int rede;
 static uint8_t taskCoreZero=0; 
 ////////////////////////////////////////////////////////////////
+const char* loginIndex = 
+ "<form name='loginForm'>"
+    "<table width='40%' bgcolor='FFF2A6' align='center'>"
+        "<tr>"
+            "<td colspan=2>"
+                "<center><font size=4><b>Ola! Indentifique-se!</b></font></center>"
+                "<br>"
+            "</td>"
+            "<br>"
+            "<br>"
+        "</tr>"
+        "<td>Login:</td>"
+        "<td><input type='text' size=25 name='userid'><br></td>"
+        "</tr>"
+        "<br>"
+        "<br>"
+        "<tr>"
+            "<td>Senha:</td>"
+            "<td><input type='Password' size=25 name='pwd'><br></td>"
+            "<br>"
+            "<br>"
+        "</tr>"
+        "<tr>"
+            "<td><input type='submit' onclick='check(this.form)' value='Identificar'></td>"
+        "</tr>"
+    "</table>"
+"</form>"
+"<script>"
+    "function check(form)"
+    "{"
+    "if(form.userid.value=='admin' && form.pwd.value=='zaq1xsw2')"
+    "{"
+    "window.open('/serverIndex')"
+    "}"
+    "else"
+    "{"
+    " alert('Login ou senha invalidos')"
+    "}"
+    "}"
+"</script>";
+const char* serverIndex = 
+"<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>"
+"<form method='POST' action='#' enctype='multipart/form-data' id='upload_form'>"
+   "<input type='file' name='update'>"
+        "<input type='submit' value='Update'>"
+    "</form>"
+ "<div id='prg'>Progresso: 0%</div>"
+ "<script>"
+  "$('form').submit(function(e){"
+  "e.preventDefault();"
+  "var form = $('#upload_form')[0];"
+  "var data = new FormData(form);"
+  " $.ajax({"
+  "url: '/update',"
+  "type: 'POST',"
+  "data: data,"
+  "contentType: false,"
+  "processData:false,"
+  "xhr: function() {"
+  "var xhr = new window.XMLHttpRequest();"
+  "xhr.upload.addEventListener('progress', function(evt) {"
+  "if (evt.lengthComputable) {"
+  "var per = evt.loaded / evt.total;"
+  "$('#prg').html('Progresso: ' + Math.round(per*100) + '%');"
+  "}"
+  "}, false);"
+  "return xhr;"
+  "},"
+  "success:function(d, s) {"
+  "console.log('Sucesso!')"
+ "},"
+ "error: function (a, b, c) {"
+ "}"
+ "});"
+ "});"
+ "</script>";
 void callback(char* topicc, byte* payload, unsigned int length){
   //retorna infoMQTT
   String topicStr=topicc;  
@@ -250,6 +326,57 @@ void statusPorta(){
   Serial.println("ENTROU NO LOOP DE ENVIAR CODIGO");
   }
 }
+void updateRemoto(){
+ /* Usa MDNS para resolver o DNS */
+    if (!MDNS.begin(host)){ 
+      //http://esp32.local
+      Serial.println("Erro ao configurar mDNS. O ESP32 vai reiniciar em 1s...");
+      delay(1000);
+      ESP.restart();        
+    }
+    Serial.println("mDNS configurado e inicializado;");
+    /* Configfura as páginas de login e upload de firmware OTA */
+    server.on("/", HTTP_GET, []() {
+      server.sendHeader("Connection", "close");
+      server.send(200, "text/html", loginIndex);
+    });
+    server.on("/serverIndex", HTTP_GET, []() {
+        server.sendHeader("Connection", "close");
+        server.send(200, "text/html", serverIndex);
+    });
+    /* Define tratamentos do update de firmware OTA */
+    server.on("/update", HTTP_POST, []() 
+    {
+        server.sendHeader("Connection", "close");
+        server.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
+        ESP.restart();
+    }, []() {
+        HTTPUpload& upload = server.upload();
+         
+        if (upload.status == UPLOAD_FILE_START) 
+        {
+            /* Inicio do upload de firmware OTA */
+            Serial.printf("Update: %s\n", upload.filename.c_str());
+            if (!Update.begin(UPDATE_SIZE_UNKNOWN)) 
+                Update.printError(Serial);
+        } 
+        else if (upload.status == UPLOAD_FILE_WRITE) 
+        {
+            /* Escrevendo firmware enviado na flash do ESP32 */
+            if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) 
+                Update.printError(Serial);      
+        } 
+        else if (upload.status == UPLOAD_FILE_END) 
+        {
+            /* Final de upload */
+            if (Update.end(true))             
+                Serial.printf("Sucesso no update de firmware: %u\nReiniciando ESP32...\n", upload.totalSize);
+            else
+                Update.printError(Serial);
+        }   
+    });
+    server.begin();
+}
 void setup(){
   Serial.begin(115200);
   EEPROM.begin(EEPROM_SIZE);
@@ -291,6 +418,7 @@ void setup(){
     NULL,          //task_handle de referencia
     taskCoreZero   //nucleo onde vai ser executado a tarefa
   );
+  updateRemoto();
 }
 void loop(){
   server.handleClient();
